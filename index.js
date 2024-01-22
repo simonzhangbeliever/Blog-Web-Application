971a18e1037a18f9b335efb48dc42d68d8255d93
Simon <932464009@qq.com>
2024-01-22 19:55:42 +1100

    add postgresql database

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
 let posts = [
     {title: "demo title", content: "demo content"}
 ];
+
 // get the directory path of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,43 +25,83 @@ app.use('/public/styles', express.static(__dirname + '/public/styles', {
     }
  }));
 
-app.get("/", (req, res) => {
+//connect AWS postgres database
+const db = new pg.Client({
+    user: "postgres",
+    host: "my-first-db-instance.cn4ussua40q5.ap-southeast-2.rds.amazonaws.com",
+    database: "blog",
+    password: "Xu19940521",
+    port: 5432,
+});
+db.connect();
+
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM posts");
+        posts = result.rows;
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'AWS Server Error' });
+    }
     res.render("home.ejs", {
-        listPosts: posts,
+        listPost: posts,
     });
 });
 
-app.get("/content/:id", (req, res) => {
-
-    const index = req.params.id;
+app.get("/content/:id", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM posts");
+        posts = result.rows;
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'AWS Server Error' });
+    }
+    const selectId = req.params.id; // selectId is string type
+    const selectPost = posts.find(({id}) => id === parseInt(selectId));
+    // console.log(posts);
+    // console.log(typeof(selectId));
+    // console.log(selectPost);
     res.render("content.ejs", {
-        selectPost: posts[index],
-        selectIndex: index,
+        selectPost: selectPost,
+        selectId: selectId,
     });
 });
 
-app.post("/create", (req, res) => {
+app.post("/create", async (req, res) => {
     const newPost = {
         title: req.body.title,
         content: req.body.content
     };
 
-    posts.push(newPost);
+    try {
+        await db.query("INSERT INTO posts (title, content) VALUES ($1, $2)", [newPost.title, newPost.content]);
+    } catch (error) {
+        console.error('Error inserting query', error);
+    }
     res.redirect("/");
 });
 
-app.post("/edit/:id", (req, res) => {
+app.post("/edit/:id", async (req, res) => { 
 
-    const index = req.params.id;
-    const content = req.body.updatedContent;
-    posts[index].content = content;
-    res.redirect(`/content/${index}`);
-
+    const selectId = req.params.id;
+    const updatedContent = req.body.updatedContent;
+    try {
+        await db.query("UPDATE posts SET content = $1 WHERE id = $2;", [updatedContent, parseInt(selectId)]);
+      } catch (error) {
+        console.error('Error updating query', error);
+      }
+    res.redirect(`/content/${selectId}`);
 });
 
-app.post("/delete/:id", (req, res) => {
-    const index = req.params.id;
-    posts.splice(index, 1);
+app.post("/delete/:id", async (req, res) => {
+    const selectId = req.params.id;
+    try {
+        await db.query("DELETE FROM posts WHERE id = $1;", [parseInt(selectId)]);
+      } catch (error) {
+        console.error('Error deleting query', error);
+      }
+
     res.redirect("/");
 });
 
